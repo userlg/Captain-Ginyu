d25d6a0faccb77effdd69e4b865f96a69a462dc3
userlg <guipeluis90@gmail.com>
2024-09-17 08:41:38 -0400

    Changes done

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+import time as t
 
+from colorama import init,Fore
+
+from src import utils as u
+
+init()
 
 if __name__ == '__main__':
-    print ('Working')
+    print(Fore.GREEN + 'Working')
+    u.show_folders()
+    t.sleep(10)
+    
---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import os
+
+from colorama import Fore
+
+
+def show_folders() -> None:
+    root = os.getcwd()
+
+    content = os.listdir(root)
+
+    files = []
+
+    for file in content:
+        if os.path.isdir(file) and not file.startswith("."):
+            files.append(file)
+
+    if len(files) > 0:
+        i = 1
+
+        for file in files:
+            os.rename(file, "new_name_" + str(i))
+            i += 1
+        print(Fore.GREEN + "Operation Successfull")
+    else:
+        print(Fore.RED + "No folders detected")
